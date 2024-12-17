7d9fbf994b18d217310f91182bcca959f85c8a85
Chandan Kheto <chandankheto46@gmail.com>
2024-12-17 11:41:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function turnOn() {
+  document.getElementById("bulb").src = "https://e7.pngegg.com/pngimages/922/441/png-clipart-creative-bulb-lightbulb-energy-saving-lamps-thumbnail.png"; // Replace with the "on" bulb image URL 
+}
+
+function turnOff() {
+  document.getElementById("bulb").src = "https://w7.pngwing.com/pngs/859/425/png-transparent-light-bulb-illustration-incandescent-light-bulb-lamp-bulb-candle-product-light-thumbnail.png"; // Replace with the "off" bulb image URL
+}
